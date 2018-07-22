c890d24db27e0d8f154dfc81b042b89deded28fc
TomaszPierzchala <t.pierzchala@gmail.com>
2018-07-22 14:35:43 +0200

    reservedSeats[] is a table of rows containing map of taken/free seats. Seats in a row are mapped as bits : 1 as a taken and 0 as a free place. Also some auxiliary methods added like printSeats(), and some are started.

--- src/main/java/eu/tp/interview_tasks/tt/Solution1.java
+++ src/main/java/eu/tp/interview_tasks/tt/Solution1.java
@@ -1,20 +1,42 @@
 package eu.tp.interview_tasks.tt;
 
+import java.util.Arrays;
+
 public class Solution1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String reservedSeats = "1A 2F 1C";
+		
+		Solution1 sol = new Solution1();
+		sol.solution(2, reservedSeats);
 
 	}
 	
-	public static int solution(int N, String S) {
-		// code in Java 8
+	public int solution(int N, String S) {
+		// table of reserved seats, row after row
+		// seats in each row are represented by bits : 1 for taken and 0 for free one
+		// eg. [100_0000_000][000_1000_001]
+		final int reservedSeats[] = new int[N];
+		
+		printSeats(reservedSeats);
 		
 		return 0;
 	}
 	
-	static void reservedSeats(String S) {
+	void reserveSeats(String S, int reservedSeats[]) {
+		String seats[] = S.split(" ");
+		Arrays.stream(seats).forEach(System.out::println);
 		
 	}
+	
+	void convertSeatToBinary(String seat, int reservedSeat[]) {
+		
+	}
+	
+	private void printSeats(int reservedSeats[]) {
+		int N = reservedSeats.length;
+		System.out.println(Arrays.toString(reservedSeats));
+	}
+	
 
 }
