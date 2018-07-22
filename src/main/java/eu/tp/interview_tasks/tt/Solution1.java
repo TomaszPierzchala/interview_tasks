9339cb9e07f58493c77f799bf146091465d31dfd
TomaszPierzchala <t.pierzchala@gmail.com>
2018-07-22 13:52:18 +0200

    Start coding

---
+++ src/main/java/eu/tp/interview_tasks/tt/Solution1.java
@@ -0,0 +1,20 @@
+package eu.tp.interview_tasks.tt;
+
+public class Solution1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int solution(int N, String S) {
+		// code in Java 8
+		
+		return 0;
+	}
+	
+	static void reservedSeats(String S) {
+		
+	}
+
+}
