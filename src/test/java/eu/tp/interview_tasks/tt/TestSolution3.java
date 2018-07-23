a55808cb0d29f0d0670330d5958d5070f48edf8b
TomaszPierzchala <t.pierzchala@gmail.com>
2018-07-23 15:11:18 +0200

    TDD : more tests - ups - I have found now my code is not perfect (Machine should NOT allow push number bigger then MAX, as well as should properly handle (i.e. returning "-1") in case of an empty stack. I will correct the code NOW. TDD in all its glory.

--- src/test/java/eu/tp/interview_tasks/tt/TestSolution3.java
+++ src/test/java/eu/tp/interview_tasks/tt/TestSolution3.java
@@ -23,12 +23,48 @@ public class TestSolution3 {
 		//
 		assertEquals(-1, result);	
 	
-		
+		// an underflow error
 		operationsSequence = "3 DUP 5 - -";
+		//
+		result = sol.solution(operationsSequence);
+		//
+		assertEquals(-1, result);
+		
+		// an overflow error
+		operationsSequence = "1048575 1 +";
 		//
 		result = sol.solution(operationsSequence);
 		//
 		assertEquals(-1, result);	
 
+		
+		// the limit operations
+		operationsSequence = "0 1048575 -" ;
+		result = sol.solution(operationsSequence);
+		assertEquals(1048575, result);	
+		//
+		operationsSequence = "0 1048575 +" ;
+		result = sol.solution(operationsSequence);
+		assertEquals(1048575, result);
+		//
+		// as well as adding commutativity
+		operationsSequence = "1048575 0 +" ;
+		result = sol.solution(operationsSequence);
+		assertEquals(1048575, result);	
+		//
+		operationsSequence = "1048575 DUP -" ;
+		result = sol.solution(operationsSequence);
+		assertEquals(0, result);	
+		//
+		
+		// Machine should NOT allow push number bigger then MAX = 1048575 (0b11_111_11_111_11_111_11_111)
+		operationsSequence = "91048575 DUP -" ;
+		result = sol.solution(operationsSequence);
+		assertEquals(-1, result);
+		
+		//an empty stack error
+		operationsSequence = "75 POP" ;
+		result = sol.solution(operationsSequence);
+		assertEquals(-1, result);
 	}
 }
