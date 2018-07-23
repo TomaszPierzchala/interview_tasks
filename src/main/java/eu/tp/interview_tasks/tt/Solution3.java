dacc76dc050b5c6f219baaa2c62b44bba6244251
TomaszPierzchala <t.pierzchala@gmail.com>
2018-07-23 15:31:41 +0200

    proper behavior when empty stack at the end of the machine operation; as well as code formatting.

--- src/main/java/eu/tp/interview_tasks/tt/Solution3.java
+++ src/main/java/eu/tp/interview_tasks/tt/Solution3.java
@@ -6,53 +6,61 @@ public class Solution3 {
 
 	public static void main(String[] args) {
 		String operationsSequence = "13 DUP 4 POP 5 DUP + DUP + -";
-		
+
 		Solution3 sol = new Solution3();
-		
-		System.out.println("For a given operations sequence : \"" 
-				+ operationsSequence 
+
+		System.out.println("For a given operations sequence : \"" + operationsSequence
 				+ "\"\nthe Word Machine returns : " + sol.solution(operationsSequence));
-		
+
 	}
-	
+
 	public int solution(String S) {
-        // write your code in Java SE 8
+		// write your code in Java SE 8
 		String MAX_BIN_PLUS_ONE = "1 00 000  00 000  00 000  00 000";
-		Integer MAX = Integer.parseInt(MAX_BIN_PLUS_ONE.replaceAll("\\s+",""), 2) - 1;
-		
-		//System.out.println("MAX = "+ MAX);
+		Integer MAX = Integer.parseInt(MAX_BIN_PLUS_ONE.replaceAll("\\s+", ""), 2) - 1;
+
+		// System.out.println("MAX = "+ MAX);
 		Stack<Integer> stack = new Stack();
-		String [] command = S.split(" ");
-		for(int i=0; i<command.length; i++) {
-			try {
+		
+		String[] command = S.split(" ");
+		
+		Integer result = -1;
+		
+		try {
+			for (int i = 0; i < command.length; i++) {
 				switch (command[i]) {
-				case "DUP": 
+				case "DUP":
 					stack.push(stack.peek());
 					break;
 				case "POP":
 					stack.pop();
 					break;
 				case "+":
-					Integer add = stack.pop()+stack.pop();
-					if(add>MAX) throw new RuntimeException("Overflow Error");
+					Integer add = stack.pop() + stack.pop();
+					if (add > MAX)
+						throw new RuntimeException("Overflow Error");
 					stack.push(add);
 					break;
 				case "-":
-					Integer sub = stack.pop()-stack.pop();
-					if(sub<0) throw new RuntimeException("Underflow Error");
+					Integer sub = stack.pop() - stack.pop();
+					if (sub < 0)
+						throw new RuntimeException("Underflow Error");
 					stack.push(sub);
 					break;
 				default:
 					Integer number = Integer.valueOf(command[i]);
-					if(number<0 || number>MAX) throw new RuntimeException("Machine input out of range Error");
+					if (number < 0 || number > MAX)
+						throw new RuntimeException("Machine input out of range Error");
 					stack.push(number);
 					break;
 				}
-			} catch (RuntimeException rexp) {
-				return -1;
 			}
-		}
+
+			result = stack.peek();
 			
-		return stack.peek();
-    }
+		} catch (RuntimeException rexp) {
+			return -1;
+		}
+		return result;
+	}
 }
