113ff6f23d95c5952ffd546dcbc49cd7a15a15e1
TomaszPierzchala <t.pierzchala@gmail.com>
2018-07-24 10:35:21 +0200

    start JUnit TDD of task 2

---
+++ src/test/java/eu/tp/interview_tasks/tt/TestSolution2.java
@@ -0,0 +1,15 @@
+package eu.tp.interview_tasks.tt;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestSolution2 {
+
+	@Test
+	public void TestSolution() {
+//		Solution2 sol = new Solution2();
+		fail();
+		
+	}
+}
