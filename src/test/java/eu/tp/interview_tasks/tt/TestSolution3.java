18311b2bfaa7a66b267a6b0bf6b80ca09d225e03
TomaszPierzchala <t.pierzchala@gmail.com>
2018-07-24 10:22:26 +0200

    minor editor changes

--- src/test/java/eu/tp/interview_tasks/tt/TestSolution3.java
+++ src/test/java/eu/tp/interview_tasks/tt/TestSolution3.java
@@ -21,7 +21,7 @@ public class TestSolution3 {
 		//
 		result = sol.solution(operationsSequence);
 		//
-		assertEquals(-1, result);	
+		assertEquals(-1, result);
 	
 		// an underflow error
 		operationsSequence = "3 DUP 5 - -";
@@ -39,31 +39,31 @@ public class TestSolution3 {
 
 		
 		// the limit operations
-		operationsSequence = "0 1048575 -" ;
+		operationsSequence = "0 1048575 -";
 		result = sol.solution(operationsSequence);
 		assertEquals(1048575, result);	
 		//
-		operationsSequence = "0 1048575 +" ;
+		operationsSequence = "0 1048575 +";
 		result = sol.solution(operationsSequence);
 		assertEquals(1048575, result);
 		//
 		// as well as adding commutativity
-		operationsSequence = "1048575 0 +" ;
+		operationsSequence = "1048575 0 +";
 		result = sol.solution(operationsSequence);
 		assertEquals(1048575, result);	
 		//
-		operationsSequence = "1048575 DUP -" ;
+		operationsSequence = "1048575 DUP -";
 		result = sol.solution(operationsSequence);
 		assertEquals(0, result);	
 		//
 		
 		// Machine should NOT allow push number bigger then MAX = 1048575 (0b11_111_11_111_11_111_11_111)
-		operationsSequence = "91048575 DUP -" ;
+		operationsSequence = "91048575 DUP -";
 		result = sol.solution(operationsSequence);
 		assertEquals(-1, result);
 		
 		//an empty stack error
-		operationsSequence = "75 POP" ;
+		operationsSequence = "75 POP";
 		result = sol.solution(operationsSequence);
 		assertEquals(-1, result);
 	}
