4d4827f61f503c00f190ce3ca0231a5e83e67dc8
TomaszPierzchala <t.pierzchala@gmail.com>
2018-07-25 18:06:53 +0200

    updated test comment

--- src/test/java/eu/tp/interview_tasks/tt/TestSolution2.java
+++ src/test/java/eu/tp/interview_tasks/tt/TestSolution2.java
@@ -27,6 +27,7 @@ public class TestSolution2 {
 		 * [ 2,		2, 4, 2,		2,2,3] : leader = 2
 		 * [ 2,1,	  4, 2, 3,	  2,3] : leader = null
 		 * [ 2,1,3, 		2, 3, 3, 	3] : leader = 3
+		 * [ 2,1,3,1,		  3, 3, 4] : leader = null
 		 */
 		//
 		assertArrayEquals(Excpected, result);
